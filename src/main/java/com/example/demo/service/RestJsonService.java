9ab5794e79f4716e56e079f25c4c66141b660862
jooyeokkim <jooyeok42@naver.com>
2021-08-09 23:03:11 +0900

    20210809

---
+++ src/main/java/com/example/demo/service/RestJsonService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+public class RestJsonService {
+    private final String GRANT_TYPE= "authorization_code";
+    private final String CLIENT_ID = "50ca5e8cf40713abcab868ed9ed3047d";
+    private final String REDIRECT_URI= "http://kimcoder.kro.kr:8080/receiveAC";
+    private final String CLIENT_SECRET= "6hTKHMv5ZgpMvGqMwNndX238yKdR1o48";
+    private final String TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+
+    public String getAccessTokenJsonData(String code){
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpEntity request = new HttpEntity(headers);
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(TOKEN_URL)
+                .queryParam("grant_type", GRANT_TYPE)
+                .queryParam("client_id", CLIENT_ID)
+                .queryParam("redirect_uri", REDIRECT_URI)
+                .queryParam("code", code)
+                .queryParam("client_secret", CLIENT_SECRET);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                uriComponentsBuilder.toUriString(),
+                HttpMethod.POST,
+                request,
+                String.class
+        );
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return responseEntity.getBody();
+        }
+        return "error";
+    }
+}
